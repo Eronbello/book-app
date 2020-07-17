c916d590e58d183e10e068d9ffb24172dfd45c43
Eronbello <eron.bello@gmail.com>
2020-07-17 19:15:33 -0300

    chore: add button component

---
+++ src/components/base/Button.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  button(@click="$emit('click')" :disabled="loading || disabled")
+    .loading(v-if="loading")
+      i.fa.fa-spinner.fa-spin()
+    slot(v-else)
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      required: false
+    },
+    disabled: {
+      type: Boolean,
+      required: false
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  i {
+    font-size: 23px;
+  }
+}
+</style>
