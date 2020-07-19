0c24cb37b2a6f1476daef82ea5c118f95b619604
Eronbello <eron.bello@gmail.com>
2020-07-18 23:47:38 -0300

    chore: filter books by availables and borrowed on home page

--- src/store/modules/books/actions.js
+++ src/store/modules/books/actions.js
@@ -14,6 +14,32 @@ export default {
         });
     });
   },
+  setDataAvailables: context => {
+    return new Promise((resolve, reject) => {
+      api
+        .get("/api/v1/books/availables?page=1")
+        .then(response => {
+          context.commit("setData", response.data.data);
+          resolve(response.data.data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  setDataBorroweds: context => {
+    return new Promise((resolve, reject) => {
+      api
+        .get("/api/v1/books/borroweds?page=1")
+        .then(response => {
+          context.commit("setData", response.data.data);
+          resolve(response.data.data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
   setDataByCategory: (context, category_id) => {
     return new Promise((resolve, reject) => {
       api
