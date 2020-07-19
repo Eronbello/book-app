3dc6744a6940bc35780875b506d29a9a3e64fc86
Eronbello <eron.bello@gmail.com>
2020-07-19 03:42:10 -0300

    chore: add input component

---
+++ src/components/base/Input.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .input-container
+    input.input(:id="id" :type='type', :value="value" ref="input" required='' @input="updateValue()")
+    label.label(:for='id') {{ label }}
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    updateValue() {
+      this.$emit("input", this.$refs.input.value);
+    }
+  }
+};
+</script>
