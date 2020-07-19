8018601b688321b7cb885bf955c8dd6bf9f7a3bc
Eronbello <eron.bello@gmail.com>
2020-07-19 03:42:33 -0300

    chore: add select component

---
+++ src/components/base/Select.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+.select
+  select.select-text(:value="value", @change='onChange($event.target.value)')
+    option(v-for='option in options', :value='option.id')
+      | {{option.title}}
+    span.select-highlight
+    span.select-bar
+    label.select-label Select
+
+</template>
+
+<script>
+export default {
+  props: ["value", "options"],
+  computed: {
+    selected() {
+      return this.value;
+    }
+  },
+  methods: {
+    onChange(value) {
+      if (value === "") {
+        value = null;
+      }
+      this.$emit("input", value);
+    }
+  }
+};
+</script>
